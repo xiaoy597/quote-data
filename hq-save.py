15c2f5718d6ccb05b67fd51181371e2638afaa80
xiaoy <yu.xiao@seaboxdata.com>
2017-10-25 14:45:02 +0800

    Initial commit.

---
+++ hq-save.py
@@ -0,0 +1,31 @@
+import shutil
+import time
+import sys
+import os.path
+
+source_file = sys.argv[1]
+target_dir = sys.argv[2]
+target_file = os.path.join(target_dir, os.path.basename(source_file))
+
+(tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst) = time.localtime()
+
+market_start_time1 = time.mktime((tm_year, tm_mon, tm_mday, 9, 14, 30, tm_wday, tm_yday, tm_isdst))
+market_end_time1 = time.mktime((tm_year, tm_mon, tm_mday, 11, 31, 30, tm_wday, tm_yday, tm_isdst))
+market_start_time2 = time.mktime((tm_year, tm_mon, tm_mday, 12, 59, 30, tm_wday, tm_yday, tm_isdst))
+market_end_time2 = time.mktime((tm_year, tm_mon, tm_mday, 15, 01, 30, tm_wday, tm_yday, tm_isdst))
+
+modify_time = 0
+while True:
+    if market_start_time1 < time.time() < market_end_time1 \
+            or market_start_time2 < time.time() < market_end_time2:
+        if os.path.exists(source_file):
+            new_modify_time = os.stat(source_file).st_mtime
+            now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+            if new_modify_time > modify_time:
+                new_target_file = target_file + '.' + time.strftime('%Y%m%dT%H%M%S', time.localtime(new_modify_time))
+                print '%s: Copy %s to %s ...' % (now, source_file, new_target_file)
+                shutil.copy(source_file, new_target_file)
+                modify_time = new_modify_time
+            else:
+                print '%s: File is not changed.' % now
+    time.sleep(1)
---
+++ hq-upload.py
@@ -0,0 +1,84 @@
+# coding=utf-8
+
+import paramiko
+import os
+import sys
+import time
+import shutil
+
+
+def sftp_upload(host, port, username, password, local, remote):
+    print 'Connecting to %s ...' % host
+    sf = paramiko.Transport((host, port))
+    sf.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(sf)
+    now = time.time()
+    try:
+        if os.path.isdir(local):  # 判断本地参数是目录还是文件
+            for f in os.listdir(local):  # 遍历本地目录
+                # Don't upload files modified/created within 60 seconds.
+                if os.stat(os.path.join(local, f)).st_mtime > now - 60:
+                    print 'Skipping file %s ...' % os.path.join(local, f)
+                    continue
+                print 'Uploading %s to %s ...' % (os.path.join(local, f), os.path.join(remote + '/' + f))
+                sftp.put(os.path.join(local, f), os.path.join(remote + '/' + f))  # 上传目录中的文件
+                shutil.move(os.path.join(local, f), os.path.join(backup, f))
+        else:
+            sftp.put(local, remote)  # 上传文件
+    except Exception, e:
+        print('upload exception:', e)
+    sf.close()
+
+
+def sftp_download(host, port, username, password, local, remote):
+    sf = paramiko.Transport((host, port))
+    sf.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(sf)
+    try:
+        if os.path.isdir(local):  # 判断本地参数是目录还是文件
+            for f in sftp.listdir(remote):  # 遍历远程目录
+                sftp.get(os.path.join(remote, f), os.path.join(local, f))  # 下载目录中文件
+        else:
+            sftp.get(remote, local)  # 下载文件
+    except Exception, e:
+        print('download exception:', e)
+    sf.close()
+
+
+def create_remote_dir(host, username, password, path):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, username=username, password=password, timeout=300)
+
+    print 'Checking remote directory %s ...' % path
+    cmd = 'ls %s' % path
+    (stdin, stdout, stderr) = ssh.exec_command(cmd)
+    err = stderr.readline()
+    if len(err.strip()) > 0:
+        print err
+
+        print 'Creating remote directory %s ...' % path
+        cmd = 'mkdir %s' % path
+        (stdin, stdout, stderr) = ssh.exec_command(cmd)
+
+        err = stderr.readline()
+        if len(err.strip()) > 0:
+            print err
+
+    ssh.close()
+
+
+if __name__ == '__main__':
+    host = sys.argv[1]  # 主机
+    port = 22  # 端口
+    username = 'root'  # 用户名
+    password = 'root123'  # 密码
+    local = sys.argv[2]  # 本地文件或目录
+    remote = sys.argv[3]  # 远程文件或目录
+    backup = sys.argv[4]  # 文件备份目录
+
+    while True:
+        date = time.strftime('%Y%m%d', time.localtime())
+        create_remote_dir(host, username, password, remote + '/' + date)
+        sftp_upload(host, port, username, password, local, remote + '/' + date)  # 上传
+        time.sleep(6)
