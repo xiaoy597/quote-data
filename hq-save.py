fc6b28618bf9ff0545d1267590b3d31e91de4e2a
xiaoy <yu.xiao@seaboxdata.com>
2017-10-25 14:52:04 +0800

    Do not save hq files during weekend.

--- hq-save.py
+++ hq-save.py
@@ -7,16 +7,17 @@ source_file = sys.argv[1]
 target_dir = sys.argv[2]
 target_file = os.path.join(target_dir, os.path.basename(source_file))
 
-(tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst) = time.localtime()
-
-market_start_time1 = time.mktime((tm_year, tm_mon, tm_mday, 9, 14, 30, tm_wday, tm_yday, tm_isdst))
-market_end_time1 = time.mktime((tm_year, tm_mon, tm_mday, 11, 31, 30, tm_wday, tm_yday, tm_isdst))
-market_start_time2 = time.mktime((tm_year, tm_mon, tm_mday, 12, 59, 30, tm_wday, tm_yday, tm_isdst))
-market_end_time2 = time.mktime((tm_year, tm_mon, tm_mday, 15, 01, 30, tm_wday, tm_yday, tm_isdst))
-
 modify_time = 0
 while True:
-    if market_start_time1 < time.time() < market_end_time1 \
+    (tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst) = time.localtime()
+
+    market_start_time1 = time.mktime((tm_year, tm_mon, tm_mday, 9, 14, 30, tm_wday, tm_yday, tm_isdst))
+    market_end_time1 = time.mktime((tm_year, tm_mon, tm_mday, 11, 31, 30, tm_wday, tm_yday, tm_isdst))
+    market_start_time2 = time.mktime((tm_year, tm_mon, tm_mday, 12, 59, 30, tm_wday, tm_yday, tm_isdst))
+    market_end_time2 = time.mktime((tm_year, tm_mon, tm_mday, 15, 01, 30, tm_wday, tm_yday, tm_isdst))
+
+    if tm_wday < 5 \
+            and market_start_time1 < time.time() < market_end_time1 \
             or market_start_time2 < time.time() < market_end_time2:
         if os.path.exists(source_file):
             new_modify_time = os.stat(source_file).st_mtime
